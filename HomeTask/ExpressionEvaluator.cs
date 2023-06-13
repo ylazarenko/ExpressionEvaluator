eb10f19b4c5a8bf58391cb1a6766389b18a5f73e
Yurii Lazarenko <yurii.lazarenko@toluna.com>
2023-06-13 07:49:42 +0200

    Introduced LexicalAnalyzer

--- HomeTask/ExpressionEvaluator.cs
+++ HomeTask/ExpressionEvaluator.cs
@@ -6,57 +6,9 @@ public class ExpressionEvaluator : IExpressionEvaluator
 {
     public string Evaluate(string expressionString)
     {
-        string[] tokens = Regex.Matches(expressionString, @"\d+|[()*+x]")
-            .Select(m => m.Value)
-            .ToArray();
-        IExpression tree = GetExpressionFromTokens(tokens).Item1;
+        string[] tokens = Regex.Split(expressionString, @"(\d+|[()*+x])").Where(i => !string.IsNullOrEmpty(i)).ToArray();
+        LexicalAnalyzer lexicalAnalyzer = new LexicalAnalyzer(tokens);
+        var tree = lexicalAnalyzer.Scan();
         return tree.Evaluate().ToString()!;
-    }
-
-    private Tuple<IExpression, string[]> GetExpressionFromTokens(string[] tokens, bool parent = false)
-    {
-        IExpression left = null!;
-        while (tokens.Length > 0)
-        {
-            if (tokens[0] == "(")
-            {
-                Tuple<IExpression, string[]> result = GetExpressionFromTokens(tokens.Skip(1).ToArray(), true);
-                left = result.Item1;
-                tokens = result.Item2;
-                continue;
-            }
-            if (tokens[0] == ")")
-            {
-                return new Tuple<IExpression, string[]>(left!, parent ? tokens.Skip(1).ToArray() : tokens);
-            }
-            if (int.TryParse(tokens[0], out int num))
-            {
-                left = new ElementExpression(num, 0);
-                tokens = tokens.Skip(1).ToArray();
-                continue;
-            }
-            if (tokens[0] == "x")
-            {
-                left = new ElementExpression();
-                tokens = tokens.Skip(1).ToArray();
-                continue;
-            }
-            if (tokens[0] == "+")
-            {
-                Tuple<IExpression, string[]> result = GetExpressionFromTokens(tokens.Skip(1).ToArray());
-                left = left!.Add(result.Item1);
-                tokens = result.Item2;
-                continue;
-            }
-            if (tokens[0] == "*")
-            {
-                Tuple<IExpression, string[]> result = GetExpressionFromTokens(tokens.Skip(1).ToArray());
-                left = left!.Multiply(result.Item1);
-                tokens = result.Item2;
-                continue;
-            }
-        }
-
-        return new Tuple<IExpression, string[]>(left!, tokens);
     }
 }
---
+++ HomeTask/LexicalAnalyzer.cs
@@ -0,0 +1,68 @@
+﻿namespace HomeTask;
+
+public class LexicalAnalyzer
+{
+    private readonly string[] _input;
+    private int _index;
+
+    public LexicalAnalyzer(string[] input)
+    {
+        _input = input;
+        _index = 0;
+    }
+
+    public IExpression Scan(bool paren = false)
+    {
+        IExpression left = null!;
+        while (_index < _input.Length)
+        {
+            string head = _input[_index];
+            if (head == "(")
+            {
+                Next();
+                left = Scan(true);
+                continue;
+            }
+            if (head == ")")
+            {
+                if (paren)
+                {
+                    Next();
+                }
+                return left;
+            }
+            if (int.TryParse(head, out int coef))
+            {
+                Next();
+                left = new ElementExpression { Coefficient = coef, Power = 0 };
+                continue;
+            }
+            if (head == "x")
+            {
+                Next();
+                left = new ElementExpression();
+                continue;
+            }
+            if (head == "+")
+            {
+                Next();
+                IExpression right = Scan();
+                left = left.Add(right);
+                continue;
+            }
+            if (head == "*")
+            {
+                Next();
+                IExpression right = Scan();
+                left = left.Multiply(right);
+            }
+        }
+
+        return left;
+    }
+
+    private void Next()
+    {
+        _index++;
+    }
+}
